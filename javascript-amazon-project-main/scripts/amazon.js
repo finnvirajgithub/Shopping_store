ccc8c218bcf979a7a024df93a9d9a6ad63c0f907
Sameera Rajapakshe <svirajrajapaksha18@gmail.com>
2023-06-27 23:02:33 +0530

    Cart quantity showing in page

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -86,6 +86,15 @@ document.querySelectorAll('.js-add-to-cart')
           quantity: 1
         });
       }
-      console.log(cart)
+
+      //show cart quantitity of the cart
+
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      })
+
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
     });
   });
