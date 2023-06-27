50862eda4304fa1c8a96f3163a59a45956edd3f4
Sameera Rajapakshe <svirajrajapaksha18@gmail.com>
2023-06-27 22:49:15 +0530

    Shopping Cart

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -49,15 +49,43 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
   `;
 });
 
-console.log(productsHTML);
-
 //put products on the webpage
 
 document.querySelector('.js-product-grid').innerHTML = productsHTML;
+
+//Add products to the cart
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;    //data attribute
+
+      //check product is in the cart
+
+      let cartItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId){
+          cartItem = item;
+        }
+      });
+
+      // if product in the cart increase the quantity
+
+      if (cartItem){
+        cartItem.quantity += 1;
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: 1
+        });
+      }
+      console.log(cart)
+    });
+  });
